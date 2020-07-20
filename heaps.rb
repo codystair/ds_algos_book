56972bedb2046ea7db1970a709d9b7678f0defde
Cody Stair <ccstair27@gmail.com>
2020-07-20 15:12:12 -0400

    Finish heap

--- heaps.rb
+++ heaps.rb
@@ -10,4 +10,61 @@ class Heap
   def last_node
     return @data.last
   end
+
+  def left_child_index(index)
+    return (index * 2) + 1
+  end
+
+  def right_child_index(index)
+    return (index * 2) + 2
+  end
+
+  def parent_index(index)
+    return (index - 1) / 2
+  end
+
+  def insert(value)]
+    @data << value
+    new_node_index = @data.size - 1
+
+    while new_node_index > 0 && @data[new_node_index] > @data[parent_index(new_node_index)]
+      @data[parent_index(new_node_index)], @data[new_node_index] = 
+      @data[new_node_index], @data[parent_index(new_node_index)]
+
+      new_node_index = parent_index(new_node_index)
+    end
+  end
+
+  def delete
+    @data[0] = @data.pop
+    trickle_node_index = 0
+
+    while has_greater_child(trickle_node_index)
+      larger_child_index = calculate_larger_child_index(trickle_node_index)
+
+      @data[trickle_node_index], @data[larger_child_index] = 
+      @data[larger_child_index], @data[trickle_node_index]
+
+      trickle_node_index = larger_child_index
+    end
+  end
+
+  def has_greater_child(index)
+    (@data[left_child_index(index)] &&
+     @data[left_child_index(index)] > @data[index]) || 
+    (@data[right_child_index(index)]) && 
+     @data[right_child_index(index)] > @data[index])
+  end
+
+  def calculate_larger_child_index(index)
+    if !@data[right_child_index(index)]
+      return left_child_index(index)
+    end
+
+    if @data[right_child_index(index)] > @data[left_child_index]
+      return right_child_index(index)
+    else
+      return left_child_index(index)
+    end
+  end
 end
