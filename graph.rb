9b47ded99d4178bd17640fa50fd7991af5b1a255
Cody Stair <ccstair27@gmail.com>
2020-07-23 15:03:21 -0400

    Finish graph

--- graph.rb
+++ graph.rb
@@ -79,6 +79,72 @@ class Vertex
   end
 end
 
+class WeightedGraphVertex
+  attr_accessor :value, :adjacent_vertices
+
+  def initialize(value)
+    @value = value
+    @adjacent_vertices = {}
+  end
+
+  def add_adjacent_vertex(vertex, weight)
+    @adjacent_vertices[vertex] = weight
+  end
+end
+
+def City
+  attr_accessor :name, :routes
+
+  def initialize(name)
+    @name = name
+    @routes = {}
+  end
+
+  def add_route(city, price)
+    @routes[city] = price
+  end
+end
+
+def dijkstra_shortest_path(starting_city, final_destination)
+  cheapest_prices_table = {}
+  cheapest_previous_stopover_city_table = {}
+  unvisited_cities = []
+  visited_cities = {}
+  cheapest_prices_table[starting_city.name] = 0
+  current_city = starting_city
+
+  while current_city
+    visited_cities[current_city.name] = true
+    unvisited_cities.delete(current_city)
+
+    current_city.routes.each do |adjacent_city, price|
+      unvisited_cities << adjacent_city unless visited_cities[adjacent_city.name]
+      price_through_current_city = cheapest_prices_table[current_city.name] + price
+
+      if !cheapest_prices_table[adjacent_city.name] ||
+        price_through_current_city < cheapest_prices_table[adjacent_city.name]
+        cheapest_prices_table[adjacent_city.name] = price_through_current_city
+        cheapest_previous_stopover_city_table[adjacent_city.name] = current_city.name
+      end
+    end
+
+    current_city = unvisited_cities.min do |city|
+      cheapest_prices_table[city.name]
+    end
+  end
+
+  shortest_path = []
+  current_city_name = final_destination.name
+
+  while current_city_name != starting_city.name
+    shortest_path << current_city_name
+    current_city_name = cheapest_previous_stopover_city_table[current_city_name]
+  end
+
+  shortest_path << starting_city.name
+  return shortest_path.reverse
+end
+
 alice = Vertex.new('alice')
 bob = Vertex.new('bob')
 cynthia = Vertex.new('cynthia')
@@ -87,3 +153,16 @@ alice.add_adjacent_vertex(bob)
 alice.add_adjacent_vertex(cynthia)
 bob.add_adjacent_vertex(cynthia)
 cynthia.add_adjacent_vertex(bob)
+
+atlanta = City.new("Atlanta")
+boston = City.new("Boston")
+chicago = City.new("Chicago")
+denver = City.new("Denver")
+el_paso = City.new("El Paso")
+atlanta.add_route(boston, 100)
+atlanta.add_route(denver, 160)
+boston.add_route(chicago, 120)
+boston.add_route(denver, 180)
+chicago.add_route(el_paso, 80)
+denver.add_route(chicago, 40)
+denver.add_route(el_paso, 140)
